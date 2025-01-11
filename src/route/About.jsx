837aba7e6d079182b6f586f7101524ff21531c8d
Arafat1652 <arafathosen4042@gmail.com>
2025-01-11 13:00:43 +0600

    added about work contact route

--- src/route/About.jsx
+++ src/route/About.jsx
@@ -3,7 +3,21 @@
 const About = () => {
     return (
         <div>
-            <h3 className='text text-5xl'>About Us is Running......</h3>
+           <main className="h-screen w-full flex flex-col justify-center items-center bg-white">
+	<h1 className="text-9xl font-extrabold text-black tracking-widest">404</h1>
+	<div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+		Page Not Found
+	</div>
+	<button className="mt-5">
+      <a
+        className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+      >
+        <span
+          className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+        ></span>
+      </a>
+    </button>
+</main>
         </div>
     );
 };
